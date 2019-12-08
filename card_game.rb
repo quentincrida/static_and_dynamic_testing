d7088797d73f39881454a5354189acfec4f61aac
Quentin Crida <qcrida@gmail.com>
2019-12-08 21:54:56 +0000

    Screenshots added

--- card_game.rb
+++ card_game.rb
@@ -15,13 +15,13 @@ class CardGame
   #   end
   # end
 
-  def check_for_ace(card)
-    if card.value == 1
-      return true
-    else
-      return false
-    end
-  end
+  # def check_for_ace(card)
+  #   if card.value == 1
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 #
 #   dif highest_card(card1 card2)
 #   if card1.value > card2.value
@@ -32,13 +32,13 @@ class CardGame
 # end
 # end
 
-  def highest_card(card1, card2)
-    if card1.value > card2.value
-      return card1
-    else
-      return card2
-  end
-end
+#   def highest_card(card1, card2)
+#     if card1.value > card2.value
+#       return card1
+#     else
+#       return card2
+#   end
+# end
 
 # def self.cards_total(cards)
 #   total
--- specs/card_game_spec.rb
+++ specs/card_game_spec.rb
@@ -17,17 +17,17 @@ class TestCardGame < MiniTest::Test
 
   end
 
-  def test_check_for_ace
-    assert_equal(true, @card_game.check_for_ace(@card1))
-    assert_equal(false, @card_game.check_for_ace(@card2))
-
-  end
-
-  def test_highest_card
-    assert_equal(@card2, @card_game.highest_card(@card1,@card2))
-    assert_equal(@card2, @card_game.highest_card(@card2, @card1))
-  end
-
+  # def test_check_for_ace
+  #   assert_equal(true, @card_game.check_for_ace(@card1))
+  #   assert_equal(false, @card_game.check_for_ace(@card2))
+  #
+  # end
+  #
+  # def test_highest_card
+  #   assert_equal(@card2, @card_game.highest_card(@card1,@card2))
+  #   assert_equal(@card2, @card_game.highest_card(@card2, @card1))
+  # end
+  #
   def test_cards_total
     assert_equal("You have a total of 3", CardGame.cards_total(@cards))
   end
